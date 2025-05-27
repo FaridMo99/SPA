c1e698f932e79b650917b945ba2a8d766824a350
FaridMo99 <farid-ahmad@hotmail.de>
2025-05-28 01:39:55 +0200

    added refetch on mount

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import CreatePostField from "../components/home/CreatePostField";
 import PostCard from "../components/home/PostCard";
-import getPosts from "../utils/getPosts";
+import getPosts from "../utils/getPosts"
 import { useQuery } from "@tanstack/react-query";
-import CustomLoader from "../components/CustomLoader";
+import CustomLoader from "../components/CustomLoader"
 
 function Home() {
-  const { isLoading, data, isError, error } = useQuery({
-    queryKey: ["getAllPosts"],
-    queryFn: () => getPosts(),
-  });
+  const {isLoading, data, isError, error} = useQuery({
+    queryKey:["getAllPosts"],
+    queryFn:()=>getPosts(),
+    refetchOnMount:true,
+    staleTime:0
+  })
 
-  if (isError)
-    return (
-      <p className="text-red-500 font-bold text-2xl self-center">
-        {error.message}
-      </p>
-    );
+  
 
   return (
     <>
-      <CreatePostField />
+      {!isLoading && !isError && <CreatePostField />}
       <div className="w-full flex flex-col items-center">
-        {isLoading && <CustomLoader />}
-        {!isLoading &&
-          data?.map((post) => <PostCard key={post.username} postData={post} />)}
+        {isLoading && <CustomLoader styles="mt-[42vh]"/>}
+        {!isLoading && data?.map(post=> <PostCard key={post.username} postData={post}/>)}
+        {isError && <p className="text-red-500 font-bold text-2xl">{error.message}</p>}
       </div>
     </>
   );
