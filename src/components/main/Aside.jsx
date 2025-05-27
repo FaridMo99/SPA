8f21e24702c815dc75ca7bb00b9dba5b1e56ff49
FaridMo99 <farid-ahmad@hotmail.de>
2025-05-27 18:22:48 +0200

    added retractable sidebar

--- src/components/main/Aside.jsx
+++ src/components/main/Aside.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { ArrowRightToLine, ArrowLeftToLine } from "lucide-react";
 
-function Aside({ paths }) {
+function Aside({ paths, asideOpen, setAsideOpen }) {
+  function clickHandler() {
+    setAsideOpen((pre) => {
+      const newVal = !pre;
+      sessionStorage.setItem("aside", JSON.stringify(newVal));
+      return newVal;
+    });
+  }
+
   return (
-    <aside className="h-[85vh] w-[20vw] fixed top-[15vh] left-0 border-r-2 border-r-green-300 bg-gray-50">
-      <nav className="w-full h-full flex flex-col  justify-evenly items-center font-bold">
+    <aside className={`h-[85vh] ${asideOpen ? "w-[20vw]" : "w-[10vw]"} fixed top-[15vh] left-0 border-r-2 border-r-green-300 bg-gray-50`}>
+      <nav className="w-full h-full flex flex-col relative justify-evenly items-center font-bold">
+        <button
+          aria-label={asideOpen ? "Collapse sidebar" : "Expand sidebar"}
+          onClick={clickHandler}
+          className={`${asideOpen ? "bg-gray-50 absolute top-2 left-[19.8vw] border-2 border-green-300 w-8 flex justify-center items-center rounded-r-lg h-12 z-10 hover:bg-gray-300" : "absolute top-6 hover:bg-gray-300 p-2 rounded-2xl"}`}
+        >
+          {asideOpen ? (
+            <ArrowLeftToLine className="text-green-500" />
+          ) : (
+            <ArrowRightToLine className="text-green-500" />
+          )}
+        </button>
+
         {paths?.map((path) => (
           <NavLink
-            className={({ isActive }) => (isActive ? " text-green-300" : "")}
+            className={({ isActive }) =>
+              `${isActive ? "text-green-300" : ""} hover:bg-gray-300 p-2 rounded-2xl`
+            }
             to={path.href}
             key={path.href}
           >
-            {path.name}
-            {path.icon}
+            {asideOpen ? path.name : path.icon}
           </NavLink>
         ))}
       </nav>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/main/Header";
 import Aside from "../components/main/Aside";
+import { House, UserRound } from "lucide-react";
 
 function MainLayout() {
+const [asideOpen, setAsideOpen] = useState(() => {
+  const stored = sessionStorage.getItem("aside");
+  return stored !== null ? JSON.parse(stored) : true;
+});
+  
   return (
     <>
       <Header />
       <Aside
         paths={[
-          { href: "/home", name: "Home" },
-          { href: "/profile", name: "Profile" },
+          { href: "/home", name: "Home", icon: <House /> },
+          { href: "/profile", name: "Profile", icon: <UserRound /> },
         ]}
+        asideOpen={asideOpen}
+        setAsideOpen={setAsideOpen}
       />
-      <main className="w-[80vw] mt-[15vh] h-screen bg-gray-600 ml-[20vw]">
+      <main
+        className={`${asideOpen ? "w-[80vw] ml-[20vw]" : "w-[90vw] ml-[10vw]"} mt-[15vh] h-screen bg-gray-600`}
+      >
         <Outlet />
       </main>
     </>
