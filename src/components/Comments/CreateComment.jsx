d7d97faf507a77325ab51b18e89084538e63896e
FaridMo99 <farid-ahmad@hotmail.de>
2025-06-07 22:43:08 +0200

    fixed bug which wouldnt show comment instantly

--- src/components/Comments/CreateComment.jsx
+++ src/components/Comments/CreateComment.jsx
@@ -3,7 +3,7 @@ import { Send } from "lucide-react";
 import { comment as commenting } from "../../utils/interactWithPost";
 import { useMutation } from "@tanstack/react-query";
 import useAuth from "../../stores/authStore";
-
+import { useQueryClient } from "@tanstack/react-query";
 const buttonStyles =
   "bg-green-300 text-white rounded-3xl p-2 mr-2 font-bold hover:bg-gray-300 hover:text-green-300 disabled:bg-gray-100 disabled:text-gray-400 disabled:hover:bg-gray-100 disabled:hover:text-gray-400 absolute right-1 bottom-1";
 
@@ -11,6 +11,7 @@ const formStyles =
   "w-full  outline-1 bg-gray-50 outline-gray-300 fixed bottom-0 right-0 flex items-center justify-center py-2 px-4";
 
 function CreateComment({ commentId }) {
+  const queryClient = useQueryClient();
   const [value, setValue] = useState("");
   const user = useAuth((state) => state.user);
 
@@ -19,6 +20,7 @@ function CreateComment({ commentId }) {
     mutationFn: ({ postId, username, comment, avatar }) =>
       commenting({ postId, username, comment, avatar }),
     onSuccess: () => {
+      queryClient.invalidateQueries(["get comment", commentId]);
       setValue("");
     },
   });
