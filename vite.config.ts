eb53b205e12d62e55010de294717f99761e06591
FaridMo99 <farid-ahmad@hotmail.de>
2025-10-30 00:21:24 +0100

    updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
-  },
 });
